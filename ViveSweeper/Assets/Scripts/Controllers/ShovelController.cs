2dc895791160ae76b8cda011ac3c8443faaa7425
johngillett <jgillett@pugetsound.edu>
2016-05-28 01:29:34 -0600

    Additional Controller Logic

--- ViveSweeper/Assets/Scripts/Controllers/ShovelController.cs
+++ ViveSweeper/Assets/Scripts/Controllers/ShovelController.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts.World;
+using Assets.Scripts.World.GridWorld;
 
 public class ShovelController : MonoBehaviour {
 
     [SerializeField]
     private GameObject shovel;
 
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
 
     public void disable()
     {
@@ -24,11 +28,33 @@ public class ShovelController : MonoBehaviour {
 
     void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("Digging: " + collider.gameObject.name);
+
         //DIG!
+        int y;
+
+        string name = collider.gameObject.name;
+
+        //Debug.Log("Attempting to parse: " + name);
+
+        if (int.TryParse(name, out y))
+            if (isValidGridSpaceName(y))
+            {
+                GridSpace interactSpace = WorldConstants.World.GetSpaceFromWorldIndex(y);
+                interactSpace.Interact();
+                controller.TriggerHapticPulse(500, Valve.VR.EVRButtonId.k_EButton_Axis0);
+            }
     }
 
     //void OnTriggerExit(Collider collider)
     //{
 
     //}
+
+    private bool isValidGridSpaceName(int y)
+    {
+        return (y >= 0 && y <= WorldConstants.World.TotalSize);
+    }
+
+
 }

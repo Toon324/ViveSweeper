dd2f26fffb32a8c8d0997699b401295224fb9e1f
johngillett <jgillett@pugetsound.edu>
2016-05-28 00:32:00 -0600

    Shovel Controller

---
+++ ViveSweeper/Assets/Scripts/Controllers/ShovelController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShovelController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject shovel;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void disable()
+    {
+        shovel.SetActive(false);
+    }
+
+    public void enable()
+    {
+        shovel.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //DIG!
+    }
+
+    //void OnTriggerExit(Collider collider)
+    //{
+
+    //}
+}
